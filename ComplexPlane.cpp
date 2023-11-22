62a18cc77f674dd1d06d6aed5203ec1c19d9e898
EthanW2143 <150193701+EthanW2143@users.noreply.github.com>
2023-11-22 10:51:26 -0800

    Update ComplexPlane.cpp
    Implemented final 2 functions

--- ComplexPlane.cpp
+++ ComplexPlane.cpp
@@ -91,11 +91,71 @@ size_t ComplexPlane::countIterations(Vector2f coord)
 {
 //Count # of iterations of the set for the given coordinate as specified above
   size_t iterations = 0;
-  complex z;
-  for (int i = 0; i < MAX_ITER; i++)
-    {
-      c
+  complex <float> c (coord.x, coord.y);
+  complex <float> z (0.0,0.0);
 
-  
+  while (iterations < MAX_ITER)
+    {
+      z = z * z + c;
+      if (abs(z) > 2.0)
+      {
+        return iterations;
+      }
+      iterations++;
+    }
+
+  return MAX_ITER;
+}
+
+void ComplexPlane::iterationsToRGB(size_t count, Uint8& r, Uint8& g, Uint8& b)
+{
+    Uint8 colors[5][3] = 
+    {
+        {128, 0, 128},  // Purple / Blue
+        {0, 128, 128},  // Turquoise
+        {0, 128, 0},    // Green
+        {255, 255, 0},  // Yellow
+        {255, 0, 0}     // Red
+    };
+
+    if (count == MAX_ITER)
+    {
+      r = 0;
+      g = 0;
+      b = 0;
+    }
+
+    else
+    {
+      //Creates a region based on number of iterations
+      size_t region = (count * 5) / maxIterations;
+      
+      // Calculate the color within the region by sliding the iteration count
+      
+      size_t regionIterations = count % (maxIterations / 5);
+      size_t slidingEffect = (regionIterations * 255) / (maxIterations / 5);
+
+      r = baseColors[region][0] + slidingEffect;
+      g = baseColors[region][1] + slidingEffect;
+      b = baseColors[region][2] + slidingEffect;
+    }
+
+  Vector2f ComplexPlane::mapPixelToCoords(Vector2i mousePixel)
+  {
+    // Calculate the normalized coordinates in the range [0, 1]
+    float normalizedX = (mousePixel.x / m_pixel_size.x);
+    float normalizedY = (mousePixel.y / m_pixel_size.y);
+
+    // Calculate the coordinates in the complex plane
+    float realPart = normalizedX * m_plane_size.x + (m_plane_center.x - m_plane_size.x / 2.0);
+    float imaginaryPart = normalizedY * m_plane_size.y + (m_plane_center.y - m_plane_size.y / 2.0);
+
+    return Vector2f(realPart, imaginaryPart);
+  }
+
+    
+
+
+
 }
 
